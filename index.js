7537e235234f16cb30114b586ffb1639cd749134
tangusjr <kipngenogilbert96@gmail.com>
2022-06-22 15:28:49 +0300

    commit tree 0a1a611cf084b3376e6c59f538b08e309fbde91d

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
+function wordReversed(word) {
+  return word.split("").reverse().join("");
+}
 function isPalindrome(word) {
   // Write your algorithm here
+
+  invertedWord = wordReversed(word);
+  return wordReversed === word;
+}
+
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
 }
 
 /* 
